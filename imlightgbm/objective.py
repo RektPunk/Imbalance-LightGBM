f11d7d50ade992f7c4062e4073a3612eec9533f3
RektPunk <rektpunk@gmail.com>
2024-09-15 16:45:23 +0900

    introduce binary objective

--- imlightgbm/objective.py
+++ imlightgbm/objective.py
@@ -10,23 +10,59 @@ from imlightgbm.utils import logger
 
 EvalLike = Callable[[np.ndarray, Dataset], tuple[str, float, bool]]
 ObjLike = Callable[[np.ndarray, Dataset], tuple[np.ndarray, np.ndarray]]
-ALPHA_DEFAULT: float = 0.05
-GAMMA_DEFAULT: float = 0.05
+ALPHA_DEFAULT: float = 0.25
+GAMMA_DEFAULT: float = 2.0
 OBJECTIVE_STR: str = "objective"
+IS_HIGHER_BETTER = False
+
+
+def _power(num_base: np.ndarray, num_pow: float):
+    return np.sign(num_base) * (np.abs(num_base)) ** (num_pow)
+
+
+def _log(array: np.ndarray, is_prob: bool = False) -> np.ndarray:
+    _upper = 1 if is_prob else None
+    return np.log(np.clip(array, 1e-6, _upper))
+
+
+def _sigmoid(x: np.ndarray) -> np.ndarray:
+    """Convert raw predictions to probabilities in binary task"""
+    return 1 / (1 + np.exp(-x))
 
 
 def binary_focal_eval(
     pred: np.ndarray, train_data: Dataset, alpha: float, gamma: float
 ) -> tuple[str, float, bool]:
-    is_higher_better = False
-    return "binary_focal", ..., is_higher_better
+    label = train_data.get_label()
+    pred_prob = _sigmoid(pred)
+    p_t = np.where(label == 1, pred_prob, 1 - pred_prob)
+    loss = -alpha * ((1 - p_t) ** gamma) * _log(p_t, True)
+
+    focal_loss = np.mean(loss)
+    return "binary_focal", focal_loss, IS_HIGHER_BETTER
 
 
 def binary_focal_objective(
-    pred: np.ndarray, train_data: Dataset, alpha: float, gamma: float
+    pred: np.ndarray, train_data: Dataset, gamma: float
 ) -> tuple[np.ndarray, np.ndarray]:
-    # TODO
-    return ...
+    label = train_data.get_label()
+    pred_prob = _sigmoid(pred)
+
+    # gradient
+    g1 = pred_prob * (1 - pred_prob)
+    g2 = label + ((-1) ** label) * pred_prob
+    g3 = pred_prob + label - 1
+    g4 = 1 - label - ((-1) ** label) * pred_prob
+    g5 = label + ((-1) ** label) * pred_prob
+    grad = gamma * g3 * _power(g2, gamma) * _log(g4) + ((-1) ** label) * _power(
+        g5, (gamma + 1)
+    )
+
+    # hess
+    h1 = _power(g2, gamma) + gamma * ((-1) ** label) * g3 * _power(g2, (gamma - 1))
+    h2 = ((-1) ** label) * g3 * _power(g2, gamma) / g4
+    hess = ((h1 * _log(g4) - h2) * gamma + (gamma + 1) * _power(g5, gamma)) * g1
+    return grad, hess
 
 
 def multiclass_focal_eval(
@@ -52,7 +88,7 @@ def set_fobj_feval(
             f"Invalid target type: {inferred_task}. Supported types are 'binary' or 'multiclass'."
         )
     objective_mapper: dict[str, ObjLike] = {
-        "binary": partial(binary_focal_objective, alpha=alpha, gamma=gamma),
+        "binary": partial(binary_focal_objective, gamma=gamma),
         "multiclass": partial(multiclass_focal_objective, alpha=alpha, gamma=gamma),
     }
     eval_mapper: dict[str, EvalLike] = {
