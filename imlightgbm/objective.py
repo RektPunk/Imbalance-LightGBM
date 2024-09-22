52b7b2ef10a9423c6ee391ec352f58143a598dd0
RektPunk <rektpunk@gmail.com>
2024-09-22 12:48:12 +0900

    update sklearn objective

--- imlightgbm/objective.py
+++ imlightgbm/objective.py
@@ -32,36 +32,57 @@ def _sigmoid(x: np.ndarray) -> np.ndarray:
     return 1 / (1 + np.exp(-x))
 
 
+def sklearn_binary_focal_objective(
+    y_true: np.ndarray, y_pred: np.ndarray, gamma: float
+) -> tuple[np.ndarray, np.ndarray]:
+    """Return grad, hess for binary focal objective."""
+    pred_prob = _sigmoid(y_pred)
+
+    # gradient
+    g1 = pred_prob * (1 - pred_prob)
+    g2 = y_true + ((-1) ** y_true) * pred_prob
+    g3 = pred_prob + y_true - 1
+    g4 = 1 - y_true - ((-1) ** y_true) * pred_prob
+    g5 = y_true + ((-1) ** y_true) * pred_prob
+    grad = gamma * g3 * _power(g2, gamma) * _log(g4) + ((-1) ** y_true) * _power(
+        g5, (gamma + 1)
+    )
+    # hess
+    h1 = _power(g2, gamma) + gamma * ((-1) ** y_true) * g3 * _power(g2, (gamma - 1))
+    h2 = ((-1) ** y_true) * g3 * _power(g2, gamma) / g4
+    hess = ((h1 * _log(g4) - h2) * gamma + (gamma + 1) * _power(g5, gamma)) * g1
+    return grad, hess
+
+
+def sklearn_binary_weighted_objective(
+    y_true: np.ndarray, y_pred: np.ndarray, alpha: float
+) -> tuple[np.ndarray, np.ndarray]:
+    """Return grad, hess for binary weighted objective."""
+    pred_prob = _sigmoid(y_pred)
+    grad = -(alpha**y_true) * (y_true - pred_prob)
+    hess = (alpha**y_true) * pred_prob * (1.0 - pred_prob)
+    return grad, hess
+
+
 def binary_focal_objective(
     pred: np.ndarray, train_data: Dataset, gamma: float
 ) -> tuple[np.ndarray, np.ndarray]:
     """Return grad, hess for binary focal objective."""
     label = train_data.get_label()
-    pred_prob = _sigmoid(pred)
-
-    # gradient
-    g1 = pred_prob * (1 - pred_prob)
-    g2 = label + ((-1) ** label) * pred_prob
-    g3 = pred_prob + label - 1
-    g4 = 1 - label - ((-1) ** label) * pred_prob
-    g5 = label + ((-1) ** label) * pred_prob
-    grad = gamma * g3 * _power(g2, gamma) * _log(g4) + ((-1) ** label) * _power(
-        g5, (gamma + 1)
+    grad, hess = sklearn_binary_focal_objective(
+        y_true=label,
+        y_pred=pred,
+        gamma=gamma,
     )
-
-    # hess
-    h1 = _power(g2, gamma) + gamma * ((-1) ** label) * g3 * _power(g2, (gamma - 1))
-    h2 = ((-1) ** label) * g3 * _power(g2, gamma) / g4
-    hess = ((h1 * _log(g4) - h2) * gamma + (gamma + 1) * _power(g5, gamma)) * g1
     return grad, hess
 
 
 def binary_weighted_objective(pred: np.ndarray, train_data: Dataset, alpha: float):
     """Return grad, hess for binary weighted objective."""
     label = train_data.get_label()
-    pred_prob = _sigmoid(pred)
-    grad = -(alpha**label) * (label - pred_prob)
-    hess = (alpha**label) * pred_prob * (1.0 - pred_prob)
+    grad, hess = sklearn_binary_weighted_objective(
+        y_true=label, y_pred=pred, alpha=alpha
+    )
     return grad, hess
 
 
