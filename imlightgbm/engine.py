d8b7e8a85a5b3b5ab35a792d6dabd4ede981ef40
unknown <rektpunk@gmail.com>
2026-01-12 09:18:45 +0900

    remove docstring causing syntax error

--- imlightgbm/engine.py
+++ imlightgbm/engine.py
@@ -7,7 +7,6 @@ from scipy.sparse import spmatrix
 from scipy.special import expit, softmax
 from sklearn.model_selection import BaseCrossValidator
 
-from imlightgbm.docstring import add_docstring
 from imlightgbm.objective.engine import set_params
 
 
@@ -51,7 +50,6 @@ class ImbalancedBooster(lgb.Booster):
     predict.__doc__ = lgb.Booster.predict.__doc__
 
 
-@add_docstring("train")
 def train(
     params: dict[str, Any],
     train_set: lgb.Dataset,
@@ -62,6 +60,38 @@ def train(
     keep_training_booster: bool = False,
     callbacks: list[Callable] | None = None,
 ) -> ImbalancedBooster:
+    """Perform the training with given parameters.
+    Parameters
+    ----------
+    params : dict
+        Parameters for training. Values passed through ``params`` take precedence over those
+        supplied via arguments.
+    train_set : Dataset
+        Data to be trained on.
+    num_boost_round : int, optional (default=100)
+        Number of boosting iterations.
+    valid_sets : list of Dataset, or None, optional (default=None)
+        List of data to be evaluated on during training.
+    valid_names : list of str, or None, optional (default=None)
+        Names of ``valid_sets``.
+    init_model : str, pathlib.Path, Booster or None, optional (default=None)
+        Filename of LightGBM model or Booster instance used for continue training.
+    keep_training_booster : bool, optional (default=False)
+        Whether the returned Booster will be used to keep training.
+        If False, the returned value will be converted into _InnerPredictor before returning.
+        This means you won't be able to use ``eval``, ``eval_train`` or ``eval_valid`` methods of the returned Booster.
+        When your model is very large and cause the memory error,
+        you can try to set this param to ``True`` to avoid the model conversion performed during the internal call of ``model_to_string``.
+        You can still use _InnerPredictor as ``init_model`` for future continue training.
+    callbacks : list of callable, or None, optional (default=None)
+        List of callback functions that are applied at each iteration.
+        See Callbacks in Python API for more information.
+
+    Returns
+    -------
+    booster : ImbalancedBooster
+        The trained Booster model.
+    """
     _params = set_params(params=params, train_set=train_set)
     _booster = lgb.train(
         params=_params,
@@ -77,7 +107,6 @@ def train(
     return ImbalancedBooster(model_str=_booster_str)
 
 
-@add_docstring("cv")
 def cv(
     params: dict[str, Any],
     train_set: lgb.Dataset,
@@ -97,6 +126,59 @@ def cv(
     eval_train_metric: bool = False,
     return_cvbooster: bool = False,
 ) -> dict[str, list[float] | lgb.CVBooster]:
+    """Perform the cross-validation with given parameters.
+    Parameters
+    ----------
+    params : dict
+        Parameters for training. Values passed through ``params`` take precedence over those
+        supplied via arguments.
+    train_set : Dataset
+        Data to be trained on.
+    num_boost_round : int, optional (default=100)
+        Number of boosting iterations.
+    folds : generator or iterator of (train_idx, test_idx) tuples, scikit-learn splitter object or None, optional (default=None)
+        If generator or iterator, it should yield the train and test indices for each fold.
+        If object, it should be one of the scikit-learn splitter classes
+        (https://scikit-learn.org/stable/modules/classes.html#splitter-classes)
+        and have ``split`` method.
+        This argument has highest priority over other data split arguments.
+    nfold : int, optional (default=5)
+        Number of folds in CV.
+    stratified : bool, optional (default=True)
+        Whether to perform stratified sampling.
+    shuffle : bool, optional (default=True)
+        Whether to shuffle before splitting data.
+    init_model : str, pathlib.Path, Booster or None, optional (default=None)
+        Filename of LightGBM model or Booster instance used for continue training.
+    fpreproc : callable or None, optional (default=None)
+        Preprocessing function that takes (dtrain, dtest, params)
+        and returns transformed versions of those.
+    seed : int, optional (default=0)
+        Seed used to generate the folds (passed to numpy.random.seed).
+    callbacks : list of callable, or None, optional (default=None)
+        List of callback functions that are applied at each iteration.
+        See Callbacks in Python API for more information.
+    eval_train_metric : bool, optional (default=False)
+        Whether to display the train metric in progress.
+        The score of the metric is calculated again after each training step, so there is some impact on performance.
+    return_cvbooster : bool, optional (default=False)
+        Whether to return Booster models trained on each fold through ``CVBooster``.
+
+    Returns
+    -------
+    eval_results : dict
+        History of evaluation results of each metric.
+        The dictionary has the following format:
+        {'valid metric1-mean': [values], 'valid metric1-stdv': [values],
+        'valid metric2-mean': [values], 'valid metric2-stdv': [values],
+        ...}.
+        If ``return_cvbooster=True``, also returns trained boosters wrapped in a ``CVBooster`` object via ``cvbooster`` key.
+        If ``eval_train_metric=True``, also returns the train metric history.
+        In this case, the dictionary has the following format:
+        {'train metric1-mean': [values], 'valid metric1-mean': [values],
+        'train metric2-mean': [values], 'valid metric2-mean': [values],
+        ...}.
+    """
     _params = set_params(params=params, train_set=train_set)
     return lgb.cv(
         params=_params,
