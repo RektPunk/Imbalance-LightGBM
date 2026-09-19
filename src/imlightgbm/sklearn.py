07cb6b12cf06efeb4db795186e83cfea9e3dde17
RektPunk <rektpunk@gmail.com>
2026-09-19 23:39:09 +0900

    add predict proba method

--- src/imlightgbm/sklearn.py
+++ src/imlightgbm/sklearn.py
@@ -1,9 +1,9 @@
 from collections.abc import Callable
 
 import numpy as np
-from lightgbm.sklearn import LGBMClassifier
+from lightgbm.sklearn import LGBMClassifier, LGBMModel
 from scipy.sparse import spmatrix
-from scipy.special import expit
+from scipy.special import expit, softmax
 
 from imlightgbm.objective import (
     binary_focal_objective,
@@ -27,10 +27,10 @@ class ImbalancedLGBMClassifier(LGBMClassifier):
         **kwargs,
     ) -> None:
         """Construct a gradient boosting model."""
-
+        self.objective = objective
+        self.alpha = alpha
+        self.gamma = gamma
         self.num_class = num_class
-        self.alpha = select_alpha(objective, alpha)
-        self.gamma = select_gamma(objective, gamma)
 
         if (
             objective in {"multiclass_focal", "multiclass_weighted"}
@@ -39,18 +39,13 @@ class ImbalancedLGBMClassifier(LGBMClassifier):
             raise ValueError("num_class must be provided")
 
         super().__init__(
-            objective=self.__objective_select(
-                objective,
-                self.alpha,
-                self.gamma,
-                self.num_class if isinstance(self.num_class, int) else -1,
-            ),
+            objective=objective,
             **kwargs,
         )
 
     def predict(self, *args, **kwargs) -> np.ndarray | spmatrix | list[spmatrix]:
-        """"""
-        _predict = super().predict(*args, **kwargs)
+        """Predict class labels for X."""
+        _predict = LGBMModel.predict(self, *args, **kwargs)
         if (
             kwargs.get("raw_score", False)
             or kwargs.get("pred_leaf", False)
@@ -60,11 +55,28 @@ class ImbalancedLGBMClassifier(LGBMClassifier):
             return _predict
 
         if _predict.ndim == 1:
-            return expit(_predict)
+            class_indices = (expit(_predict) >= 0.5).astype(int)
+            return self._le.inverse_transform(class_indices)
 
         return self._le.inverse_transform(np.argmax(_predict, axis=1))
 
-    def __objective_select(
+    def predict_proba(self, *args, **kwargs) -> np.ndarray | spmatrix | list[spmatrix]:
+        """Predict class probabilities for X."""
+        _predict = LGBMModel.predict(self, *args, raw_score=True, **kwargs)
+        if (
+            kwargs.get("pred_leaf", False)
+            or kwargs.get("pred_contrib", False)
+            or isinstance(_predict, spmatrix | list)
+        ):
+            return _predict
+
+        if _predict.ndim == 1:
+            p1 = expit(_predict)
+            return np.column_stack([1.0 - p1, p1])
+
+        return softmax(_predict, axis=1)
+
+    def _objective_select(
         self,
         objective: str,
         alpha: float,
@@ -95,6 +107,18 @@ class ImbalancedLGBMClassifier(LGBMClassifier):
         return _objective_mapper[objective]
 
     def _process_params(self, stage: str) -> dict:
+        _alpha = select_alpha(self.objective, self.alpha)
+        _gamma = select_gamma(self.objective, self.gamma)
+        num_class = self.num_class if isinstance(self.num_class, int) else -1
+
+        if stage == "fit":
+            self._objective = self._objective_select(
+                self.objective,
+                _alpha,
+                _gamma,
+                num_class,
+            )
+
         params = super()._process_params(stage)
         params.pop("alpha", None)
         params.pop("gamma", None)
