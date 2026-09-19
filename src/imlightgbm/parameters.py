91335126952262a3f2447b1ec5d715099de117a2
RektPunk <rektpunk@gmail.com>
2026-09-19 23:22:39 +0900

    add sklearn and iclude equal

--- src/imlightgbm/parameters.py
+++ src/imlightgbm/parameters.py
@@ -6,7 +6,7 @@ def select_alpha(objective: str, alpha: float | None) -> float:
     if objective not in {"binary_weighted", "multiclass_weighted"} or alpha is None:
         return ALPHA_DEFAULT
 
-    if alpha < 0:
+    if alpha <= 0.0:
         raise ValueError(f"Expected a positive number for alpha, but got {alpha}.")
 
     return alpha
@@ -16,7 +16,7 @@ def select_gamma(objective: str, gamma: float | None) -> float:
     if objective not in {"binary_focal", "multiclass_focal"} or gamma is None:
         return GAMMA_DEFAULT
 
-    if gamma < 0:
+    if gamma <= 0.0:
         raise ValueError(f"Expected a positive number for gamma, but got {gamma}.")
 
     return gamma
